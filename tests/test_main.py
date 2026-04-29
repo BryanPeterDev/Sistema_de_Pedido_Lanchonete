508ce90c262d8608c258937a5182467aa4ee37b9
Bryan Peter <bryandev.contato@gmail.com>
2026-04-29 17:09:49 -0300

    2.1

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-
-from http import HTTPStatus
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo(client):
-
-    response = client.get('/')  # Ação
-
-    assert response.status_code == HTTPStatus.OK
